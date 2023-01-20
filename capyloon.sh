4f36c892e69e5f16150c80ac5c0eeaa89f77dffc
Fabrice Desré <fabrice@desre.org>
2023-01-20 12:46:08 -0800

    Add support to build installer images

--- capyloon.sh
+++ capyloon.sh
@@ -1,1 +1,21 @@
-./build.sh -e capyloon -t pinephonepro -s 2>&1|tee capyloon.log
+#!/bin/bash
+
+set -e
+
+OPT=
+DEVICE=pinephonepro
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --installer)
+        OPT="$OPT -o"
+        ;;
+    --librem5)
+        DEVICE=librem5
+        ;;
+    esac
+    shift
+done
+
+echo "About to run: './build.sh ${OPT} -e capyloon -t ${DEVICE} -s'"
+time ./build.sh ${OPT} -e capyloon -t ${DEVICE} -s 2>&1|tee capyloon.log
